7967ae9fcdd0b460a13e96f19d9bb1ea6d2ad1f2
jmpham3 <jmpham3@uci.edu>
2024-02-08 23:38:47 -0800

    updates to homescreen ui, tailwind dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import "./App.css";
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LandingPage from "./LandingPage";
 
-function App() {
-  return (
-    <div>
-      <h1 className="text-6xl text-red-500">Hello, Tailwind CSS in React</h1>
-    </div>
+
+const router = createBrowserRouter([
+  {
+      path: "/",
+      element: <LandingPage />,
+  }
+]);
+
+function App(){
+  return(
+      <div>
+      <RouterProvider router={router} />
+      </div>
   );
 }
 
-export default App;
+export default App; 
+
+// function App() {
+//   return (
+//     <div>
+//       <h1 className="text-6xl text-red-500">Hello, Tailwind CSS in React</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import LandingPage from './LandingPage';
+import { useState, useEffect } from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <LandingPage /> */}
     <App />
   </React.StrictMode>
 );
